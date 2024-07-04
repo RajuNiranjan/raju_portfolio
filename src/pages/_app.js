e1276880c099f87b2d90e31f073060c7ebcb5af1
Raju <tnraju1910@gmail.com>
2024-07-04 18:15:13 +0530

    added frame motion library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,34 @@
 import NavBar from "@/components/NavBar/NavBar";
 import "@/styles/globals.css";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
-    <div className="w-full">
-      <NavBar />
-      <Component {...pageProps} />
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.router}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.75,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+          },
+          animateState: {
+            opacity: 1,
+          },
+          exitState: {},
+        }}
+        className="w-full"
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
   );
 }
