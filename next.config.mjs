9c6c443b2d56ee6e0513bebcab07f5dcf30a493e
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-10-02 23:22:45 +0530

    initialized next setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["cdn.jsdelivr.net", "skillicons.dev"],
-  },
 };
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,34 +1,5 @@
-import NavBar from "@/components/NavBar/NavBar";
 import "@/styles/globals.css";
-import { AnimatePresence, delay, motion } from "framer-motion";
-import { useRouter } from "next/navigation";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={router.router}
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        transition={{
-          duration: 0.75,
-          delay: 1,
-        }}
-        variants={{
-          initialState: {
-            opacity: 0,
-          },
-          animateState: {
-            opacity: 1,
-          },
-          exitState: {},
-        }}
-        className="w-full">
-        <NavBar />
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
-  );
+  return <Component {...pageProps} />;
 }
