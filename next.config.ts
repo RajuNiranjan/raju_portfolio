c547aa4a1c92011a08deb7db4d953c5c9b88c3ec
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-10-23 14:57:12 +0530

    added export script in package.json and next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',           // enable static HTML export
+  images: {
+    unoptimized: true,        // disable Next.js image optimization
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+module.exports = nextConfig
